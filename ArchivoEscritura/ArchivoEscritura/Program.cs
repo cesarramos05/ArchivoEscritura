9da7e4a96d4c93a21bb01ddbf6a8ef5bfc37db13
cesarramos05 <43106466+cesarramos05@users.noreply.github.com>
2022-05-31 11:44:30 -0700

    Segunda

--- ArchivoEscritura/ArchivoEscritura/Program.cs
+++ ArchivoEscritura/ArchivoEscritura/Program.cs
@@ -11,18 +11,39 @@ namespace ArchivoEscritura
     {
         static void Main(string[] args)
         {
-            StreamWriter sw = new StreamWriter("ejemplo.txt", true);
+            //StreamWriter sw = new StreamWriter("ejemplo.txt", true);
 
-                string[] lines =
+            string[] t = new string[1];
+            string nom;
+            Console.WriteLine("Ingrese nombre; ");
+            nom = Console.ReadLine();
+            t[0] = nom;
+
+           using (StreamWriter writer = new StreamWriter("Nombres.txt", true))
             {
-                "Esta linea es nueva",
-            "Esta es la segunda linea"
-            };
-           foreach(string line in lines)
-            {
-                sw.WriteLine(line);//Escribe en el archivo
+                for(int i = 0; i <t.Length; i++)
+                {
+                    writer.WriteLine(t[i].ToString());
+                }
+                writer.Close();
+
             }
-            sw.Close();
+            
+
+                /* string text;
+                 Console.WriteLine("Captura nombre a guardar: ");
+                 text = Console.ReadLine();
+
+                    /* string[] lines =
+                 {
+                     "Esta linea es nueva",
+                 "Esta es la segunda linea"
+                 };
+                foreach(string line in lines)
+                 {
+                     sw.WriteLine(line);//Escribe en el archivo
+                 }*/
+                //sw.Close();
 
             Console.WriteLine("Escribiendo en el archivo....");
             Console.ReadLine();
