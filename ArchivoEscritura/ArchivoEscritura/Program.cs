1b7e9fd78eaa71e5ce470e00f6e8cf370071fe75
cesarramos05 <43106466+cesarramos05@users.noreply.github.com>
2022-05-31 11:25:27 -0700

    Primera actualizacion

---
+++ ArchivoEscritura/ArchivoEscritura/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ArchivoEscritura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter("ejemplo.txt", true);
+
+                string[] lines =
+            {
+                "Esta linea es nueva",
+            "Esta es la segunda linea"
+            };
+           foreach(string line in lines)
+            {
+                sw.WriteLine(line);//Escribe en el archivo
+            }
+            sw.Close();
+
+            Console.WriteLine("Escribiendo en el archivo....");
+            Console.ReadLine();
+            Console.ReadKey();
+        }
+    }
+}
